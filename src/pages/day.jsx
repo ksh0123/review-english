35f7dbeeebc7a9c5762360a5fe0c3d49661156f8
ksh0123 <shnae.k@gmail.com>
2023-11-29 16:02:55 +0900

    Add text-to-speech

--- src/pages/day.jsx
+++ src/pages/day.jsx
@@ -3,6 +3,7 @@ import englishData from "../englishData.json";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoChevronBackCircleOutline } from "react-icons/io5";
+import axios from "axios";
 
 const Day = () => {
   const [dailyData, setDailyData] = useState();
@@ -23,6 +24,41 @@ const Day = () => {
       : setCurrentPage(currentPage - 1);
   };
 
+  const onClickSound = async () => {
+    try {
+      const response = await axios.post(
+        `https://texttospeech.googleapis.com/v1/text:synthesize?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          input: {
+            text: dailyData.sentences[currentPage].english,
+          },
+          voice: {
+            languageCode: "en-US",
+            name: "en-US-News-L",
+            ssmlGender: "FEMALE",
+          },
+          audioConfig: {
+            audioEncoding: "MP3",
+          },
+        }
+      );
+
+      const binaryData = atob(response.data.audioContent);
+
+      const byteArray = new Uint8Array(binaryData.length);
+      for (let i = 0; i < binaryData.length; i++) {
+        byteArray[i] = binaryData.charCodeAt(i);
+      }
+
+      const blob = new Blob([byteArray.buffer], { type: "audio/mp3" });
+      const newAudio = new Audio(URL.createObjectURL(blob));
+      document.body.appendChild(newAudio);
+      newAudio.play();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
     setDailyData(englishData[+day - 1]);
   }, [day]);
@@ -32,13 +68,11 @@ const Day = () => {
   if (!dailyData) return <div>Loading...</div>;
 
   return (
-    <div className="container relatives">
-      <div className="absolute top-0 left-0 p-8">
-        <Link to="/">
-          <IoChevronBackCircleOutline className=" scale-150" />
-        </Link>
-      </div>
-      <h1 className="text-center text-2xl font-semibold font-jalnan">
+    <div className="container relative">
+      <Link to="/" className="absolute top-0 left-0 p-8">
+        <IoChevronBackCircleOutline className="scale-150" />
+      </Link>
+      <h1 className="text-center text-2xl font-semibold border-b-2">
         Day {dailyData.day} - {dailyData.title}
       </h1>
       <div className="mt-12 whitespace-pre-line ">
@@ -56,7 +90,9 @@ const Day = () => {
           <button className="btn-style ml-2" onClick={onClickNext}>
             Next
           </button>
-          <button className="btn-style ml-2">Sound</button>
+          <button className="btn-style ml-2" onClick={onClickSound}>
+            Sound
+          </button>
         </div>
       </div>
     </div>
